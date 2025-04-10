e809974e13a837ba22d2331292a589f3598184aa
Chiayi Wu <cwu423@gmail.com>
2025-04-09 22:31:48 -0700

    add navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,21 @@
 const Home = () => {
   return (
-    <div className="flex h-screen w-screen items-center justify-center">
-      Hello World
+    <div className="h-screen w-screen items-center justify-center">
+      <section id="landing" className="h-screen pt-[15vh]">
+        Landing
+      </section>
+
+      <section id="about" className="h-screen pt-[15vh]">
+        About
+      </section>
+
+      <section id="mission" className="h-screen pt-[15vh]">
+        Mission
+      </section>
+
+      <section id="events" className="h-screen pt-[15vh]">
+        Events
+      </section>
     </div>
   );
 };
