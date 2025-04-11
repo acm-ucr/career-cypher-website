eba7c27313669bd8a6b65db86f20e5dfdc48bb92
Phoebe Zhou <phoebezhou@Phoebes-MacBook-Air.local>
2025-04-11 19:21:22 -0400

    phoebe_club_description

---
+++ src/components/about/description.tsx
@@ -0,0 +1,15 @@
+const Description = () => {
+  return (
+    <div className="font-poppins mx-auto my-8 flex max-w-[80%] justify-center text-center text-[3vw]">
+      <p>
+        Boost <span style={{ color: "#B97DC6" }}>skills</span> with mock
+        <br />
+        interviews & <span style={{ color: "#B1D7E4" }}>challenges</span>
+        <br />
+        in our club.
+      </p>
+    </div>
+  );
+};
+
+export default Description;
