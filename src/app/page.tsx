8deba6ffa90931d24a39fac5346fe5586f707af3
Hans <hanschundekad@gmail.com>
2025-04-14 22:31:53 -0700

    <supporting text with styling>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
+import HomeSupportingTxt from "@/components/home/Home";
+
 const Home = () => {
   return (
-    <div className="flex h-screen w-screen items-center justify-center">
-      Hello World
+    <div className="flex h-screen w-screen max-w-75 items-center justify-center place-self-center text-center text-gray-500">
+      <HomeSupportingTxt />
     </div>
   );
 };
---
+++ src/components/home/Home.tsx
@@ -0,0 +1,11 @@
+const HomeSupportingTxt = () => {
+  return (
+    <p>
+      {" "}
+      Gain confidence to impress in tough interviews. Fine-tune abilities to
+      shine & get job offers!
+    </p>
+  );
+};
+
+export default HomeSupportingTxt;
