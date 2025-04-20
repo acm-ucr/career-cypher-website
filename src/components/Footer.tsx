150453b8c4f1cf3bb4035d7863166ceb755aa5a6
Vivian Tang <ziyuetang@Vivians-MacBook-Air.local>
2025-04-20 13:00:55 -0400

    vivian/footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,27 +2,28 @@ import React from "react";
 import footerLinks from "@/data/footerLinks.json";
 import { FaInstagram, FaDiscord } from "react-icons/fa";
 import { MdOutlineMail } from "react-icons/md";
+import LogoIcon from "@/public/LogoIcon.webp";
 import Image from "next/image";
 
 const Footer = () => {
   return (
-    <footer className="fixed right-0 bottom-0 left-0 bg-transparent p-4">
-      <div className="flex justify-center space-x-6">
+    <footer className="fixed right-0 bottom-0 left-0 p-4">
+      <div className="flex justify-between items-center">
         <Image
-          src="/logoIcon"
+          src={LogoIcon}
           alt="Logo"
-          width={96}
-          height={32}
-          className="object-contain"
+          className="w-5 h-5"
         />
 
-        {footerLinks.map((item, index) => (
-          <a key={index} href={item.link}>
-            {item.text === "Instagram" && <FaInstagram className="text-lg" />}
-            {item.text === "Discord" && <FaDiscord className="text-lg" />}
-            {item.text === "Email" && <MdOutlineMail className="text-lg" />}
-          </a>
-        ))}
+        <div className="flex space-x-6">
+          {footerLinks.map((item, index) => (
+            <a key={index} href={item.link}>
+              {item.text === "Instagram" && <FaInstagram className="text-lg" />}
+              {item.text === "Discord" && <FaDiscord className="text-lg" />}
+              {item.text === "Email" && <MdOutlineMail className="text-lg" />}
+            </a>
+          ))}
+        </div>
       </div>
     </footer>
   );
