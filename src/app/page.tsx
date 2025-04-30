26d9ee7774fa3797b36a0778de17c66b3c909ee1
Jacob <110055706+frog187man@users.noreply.github.com>
2025-04-30 11:41:25 -0700

    added mission statement

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,27 @@
 import Header from "@/components/home/header";
 import AboutHeader from "@/components/about/about-header";
+import Mission from "@/components/about/mission";
 
 const Home = () => {
   return (
-    <div className="h-screen w-screen items-center justify-center">
-      <section id="landing" className="h-screen pt-[15vh]">
+    <div className="w-screen items-center justify-center">
+      <section id="landing" className="pt-[15vh]">
         <Header />
       </section>
 
-      <section id="about" className="h-screen pt-[15vh]">
+      <section
+        id="about"
+        className="flex flex-col items-center justify-center pt-[15vh]"
+      >
         <AboutHeader />
-        About
+        <Mission />
       </section>
 
-      <section id="events" className="h-screen pt-[15vh]">
+      <section id="events" className="pt-[15vh]">
         Events
       </section>
 
-      <section id="board" className="h-screen pt-[15vh]">
+      <section id="board" className="pt-[15vh]">
         Board
       </section>
     </div>
