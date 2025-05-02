ea9f93ae913ddf636db267a82a96542dc96ed984
Hans <hanschundekad@gmail.com>
2025-05-02 16:42:56 -0700

    Supporting txt

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -1,7 +1,7 @@
 const HomeSupportingTxt = () => {
   return (
     <div className="item-center flex flex-col justify-center">
-      <p className="max-w-75 items-center justify-center place-self-center text-center text-gray-500">
+      <p className="max-w-75 items-center justify-center text-center text-gray-500">
         Gain confidence to impress in tough interviews. Fine-tune abilities to
         shine & get job offers!
       </p>
