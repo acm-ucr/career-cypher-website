d534726ee5b0ddc2b4f558b88ad03395a4df5cf9
Hans <hanschundekad@gmail.com>
2025-05-02 16:55:53 -0700

    supporting txt

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,27 @@
+import Header from "@/components/home/header";
+import AboutHeader from "@/components/about/about-header";
 import HomeSupportingTxt from "@/components/home/Home";
 
 const Home = () => {
   return (
-    <div className="">
-      <HomeSupportingTxt />
+    <div className="h-screen w-screen items-center justify-center">
+      <section id="landing" className="h-screen pt-[15vh]">
+        <Header />
+        <HomeSupportingTxt />
+      </section>
+
+      <section id="about" className="h-screen pt-[15vh]">
+        <AboutHeader />
+        About
+      </section>
+
+      <section id="events" className="h-screen pt-[15vh]">
+        Events
+      </section>
+
+      <section id="board" className="h-screen pt-[15vh]">
+        Board
+      </section>
     </div>
   );
 };
