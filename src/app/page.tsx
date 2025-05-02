1f45373f66283ad810119fda0946b523a3fd30b5
Hans <hanschundekad@gmail.com>
2025-05-02 16:36:18 -0700

    Supportin txt

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import HomeSupportingTxt from "@/components/home/Home";
 
 const Home = () => {
   return (
-    <div className="flex h-screen w-screen max-w-75 items-center justify-center place-self-center text-center text-gray-500">
+    <div className="">
       <HomeSupportingTxt />
     </div>
   );
--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -1,10 +1,11 @@
 const HomeSupportingTxt = () => {
   return (
-    <p>
-      {" "}
-      Gain confidence to impress in tough interviews. Fine-tune abilities to
-      shine & get job offers!
-    </p>
+    <div className="item-center flex flex-col justify-center">
+      <p className="max-w-75 items-center justify-center place-self-center text-center text-gray-500">
+        Gain confidence to impress in tough interviews. Fine-tune abilities to
+        shine & get job offers!
+      </p>
+    </div>
   );
 };
 
