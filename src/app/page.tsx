8fef5e69955cef2ba70a5ccaded30d0d8748eeae
Vivian Tang <ziyuetang@Vivians-MacBook-Air.local>
2025-05-04 13:50:34 -0400

    new branch

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,29 @@
+import Header from "@/components/home/header";
+import AboutHeader from "@/components/about/about-header";
+import Mission from "@/components/about/mission";
+
 const Home = () => {
   return (
-    <div className="flex h-screen w-screen items-center justify-center">
-      Hello World
+    <div className="w-screen items-center justify-center">
+      <section id="landing" className="h-screen pt-[15vh]">
+        <Header />
+      </section>
+
+      <section
+        id="about"
+        className="flex h-screen flex-col items-center justify-center pt-[15vh]"
+      >
+        <AboutHeader />
+        <Mission />
+      </section>
+
+      <section id="events" className="h-screen pt-[15vh]">
+        Events
+      </section>
+
+      <section id="board" className="h-screen pt-[15vh]">
+        Board
+      </section>
     </div>
   );
 };
