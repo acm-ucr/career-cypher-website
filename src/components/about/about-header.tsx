50a8a470779f8b646a371d5db63d7057a2c9cfc9
Ampherost <Ampherost235@gmail.com>
2025-05-23 16:49:30 -0700

    Made UI and styling consistent

--- src/components/about/about-header.tsx
+++ src/components/about/about-header.tsx
@@ -1,6 +1,6 @@
 const AboutHeader = () => {
   return (
-    <div className="flex min-h-[20vh] items-center justify-center">
+    <div className="flex min-h-[10vh] items-center justify-center">
       <p className="rounded-md border border-black bg-gradient-to-b from-blue-100 to-white px-32 py-2 text-xl text-black transition-all hover:bg-black">
         ABOUT US
       </p>
--- src/components/about/mission.tsx
+++ src/components/about/mission.tsx
@@ -1,11 +1,11 @@
 const Mission = () => {
   return (
-    <div className="font-poppins flex h-2/3 w-2/3 flex-col items-center justify-center gap-11 text-center md:w-1/2">
+    <div className="font-poppins mx-auto flex max-w-4xl flex-col items-center justify-center gap-10 text-center text-xl">
       <div className="rounded-md border border-black bg-gradient-to-b from-blue-100 to-white px-4 py-2 text-base text-black transition-all hover:bg-black md:px-12 md:text-xl">
         OUR MISSION & VISION STATEMENT
       </div>
 
-      <div className="">
+      <div className="pb-10">
         Our mission is to provide a structured and competitive environment where
         individuals can master coding challenges, refine their problem-solving
         skills, and gain hands-on experience through mock interviews with
