12edc9a524807af10adee4cfb2ad7407f0d5241f
Felix Diaz <felixmdm321@gmail.com>
2025-05-29 15:15:24 -0700

    site UI updates & general bug fixes

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,27 +1,29 @@
 import React from "react";
 import { footerLinks } from "@/data/footerLinks";
-import { FaInstagram, FaDiscord } from "react-icons/fa";
-import { MdOutlineMail } from "react-icons/md";
 import LogoIcon from "@/public/LogoIcon.webp";
 import Image from "next/image";
+import Link from "next/link";
 
 const Footer = () => {
   return (
-    <footer className="fixed right-0 bottom-0 left-0 border-t-1 bg-amber-50 p-4">
+    <div className="fixed right-0 bottom-0 left-0 z-10 w-screen border-t-1 border-black bg-amber-50 p-4">
       <div className="flex items-center justify-between">
         <Image src={LogoIcon} alt="Logo" className="h-6 w-6.5" />
 
-        <div className="flex space-x-5">
+        <div className="mr-4 flex space-x-5">
           {footerLinks.map((item, index) => (
-            <a key={index} href={item.link} target="_blank">
-              {item.text === "Instagram" && <FaInstagram className="text-xl" />}
-              {item.text === "Discord" && <FaDiscord className="text-xl" />}
-              {item.text === "Email" && <MdOutlineMail className="text-xl" />}
-            </a>
+            <Link
+              key={index}
+              href={item.link}
+              target="_blank"
+              className="text-3xl hover:-translate-y-1"
+            >
+              {item.icon}
+            </Link>
           ))}
         </div>
       </div>
-    </footer>
+    </div>
   );
 };
 
