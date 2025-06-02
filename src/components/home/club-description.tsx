e9725dff98a2c14e2a3ee38890a7282ce2c7f93d
Felix Diaz <felixmdm321@gmail.com>
2025-06-02 09:01:07 -0700

    spacing

--- src/components/home/club-description.tsx
+++ src/components/home/club-description.tsx
@@ -1,7 +1,7 @@
 const ClubDescription = () => {
   return (
-    <div className="flex min-h-[20vh] flex-col items-center justify-center">
-      <p className="max-w-sm text-center text-3xl leading-relaxed">
+    <div className="font-poppins flex min-h-[20vh] flex-col items-center justify-center">
+      <p className="my-[10%] max-w-sm text-center text-3xl leading-relaxed">
         Boost <span className="text-purple-500">skills</span> with mock
         interviews &amp; <span className="text-blue-300">challenges</span> in
         our club.
