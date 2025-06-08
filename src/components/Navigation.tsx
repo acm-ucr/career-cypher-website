f700e31992e7b5351ec920708997f8ffd87e38d0
av12122 <pv144155@gmail.com>
2025-06-08 02:38:36 -0700

    fixed responsiveness

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -3,11 +3,11 @@ import Link from "next/link";
 
 const Navigation = () => {
   return (
-    <div className="fixed z-10 flex w-screen pb-9 justify-center border-b-1 border-black bg-amber-50 md:py-[1%]">
+    <div className="fixed z-10 flex w-screen justify-center border-b-1 border-black bg-amber-50 pb-9 md:py-[1%]">
       {navItems.map((navItem, index) => (
         <Link
           key={index}
-          className="md:w-[10vw] w-30 text-center text-lg md:text-[1.4vw] translate-y-4 md:translate-y-0 font-extrabold text-black"
+          className="w-30 translate-y-4 text-center text-lg font-extrabold text-black md:w-[10vw] md:translate-y-0 md:text-[1.4vw]"
           href={navItem.link}
         >
           {navItem.name}
